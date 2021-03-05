a49761f87d9909a82b0aa240dcacd94fd9f842c8
Erin <48696244+summerysaturn@users.noreply.github.com>
2021-03-05 22:30:57 +0000

    style: :art: add new sprites, leadup to core logic build

--- Assets/Scripts/View/CameraController.cs
+++ Assets/Scripts/View/CameraController.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-  //public Vector2 ScreenSize;
+  // public so we can access this for AI
   public float offset;
 
-  // line(2520,0), (1920, -2)
-  // this is used to keep the ship in frame no matter the screen height
-  // this seems broken at higher resolution widths (i.e. 1440x2560). more
-  // investigation needed. out of scope for this project.
+  public float shipOffset = 8f;
+  public float shipPadding = 1.375f;
 
   // Update is called once per frame
   void Update()
@@ -24,7 +21,11 @@ public class CameraController : MonoBehaviour
     //  Camera.main.pixelWidth
     //);
 
-    offset = (0.0033333f * Camera.main.pixelHeight) - 8.4f;
+    // this gets the current orthographic size of the camera, in vertical units,
+    // then subtracts the offset and the padding. These two values are used to /
+    // center the ship at the bottom of the frame by an equal amount of pixels /
+    // regardless of camera height.
+    offset = Camera.main.orthographicSize - shipOffset - shipPadding;
 
     transform.position = new Vector3(0,
     offset,
